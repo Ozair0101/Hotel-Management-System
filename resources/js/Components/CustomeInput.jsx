505ba75e583f3be3172536e2589575a1ea21d792
Ozair0101 <ozairjan186@gmail.com>
2025-09-21 16:35:57 +0430

    inpuy

--- resources/js/Components/CustomeInput.jsx
+++ resources/js/Components/CustomeInput.jsx
@@ -23,7 +23,12 @@ const CustomeInput = ({
                 }`}
             />
             {meta.touched && meta.error ? (
-                <div className="error-text">{meta.error}</div>
+                <div className="form-error flex items-center mt-1">
+                    <svg className="w-4 h-4 inline mr-1.5 flex-shrink-0" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+                        <path fillRule="evenodd" d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7 4a1 1 0 11-2 0 1 1 0 012 0zm-1-9a1 1 0 00-1 1v4a1 1 0 102 0V6a1 1 0 00-1-1z" clipRule="evenodd"></path>
+                    </svg>
+                    <span>{meta.error}</span>
+                </div>
             ) : null}
         </div>
     );
